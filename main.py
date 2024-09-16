cf1cd852ab215f9b90b6abd0f3035b9ab9c5c696
BasdB97 <basdebruin97@gmai.com>
2024-09-16 14:01:32 +0200

    Added logic that splits asteroids in 2 smaller asteroids and sents them in 2 random directions when theyre hit

--- main.py
+++ main.py
@@ -11,54 +11,51 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-    
+
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-    
+
     Player.containers = (updatable, drawable)
-    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
-    
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
-    
-    AsteroidField.containers = updatable
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        for obj in updatable:          
+
+        for obj in updatable:
             obj.update(dt)
-        
+
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
-            
-        for asteroid in asteroids:
+
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
                     shot.kill()
-        
-        screen.fill((0, 0, 0))
-        
+                    asteroid.split()
+
+        screen.fill("black")
+
         for obj in drawable:
             obj.draw(screen)
 
         pygame.display.flip()
-        
-        # limit the FPS to 60
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
--- shot.py
+++ shot.py
@@ -7,7 +7,6 @@ class Shot(CircleShape):
         super().__init__(x, y, SHOT_RADIUS)
         
     def draw(self, screen):
-        
         pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, 2)
         
     def update(self, dt):
